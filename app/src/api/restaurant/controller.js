5d7ce4a748b315da5fd155984cf2f911d1d304e2
utta <111429116+Uttamoosa@users.noreply.github.com>
2026-04-29 14:33:19 +0300

    editorconfig
    vaihoin js filet 4 space eikä 2

--- app/src/api/restaurant/controller.js
+++ app/src/api/restaurant/controller.js
@@ -1,49 +1,49 @@
 import promisePool from '../../utils/database.js';
 
 const getList = async (req, res) => {
-    const [result] = await promisePool.execute('SELECT * FROM meals');
-    res.json(result);
+	const [result] = await promisePool.execute('SELECT * FROM meals');
+	res.json(result);
 };
 
 const getReviews = async (req, res) => {
-    const [result] = await promisePool.execute('SELECT * FROM reviews');
-    res.json(result);
+	const [result] = await promisePool.execute('SELECT * FROM reviews');
+	res.json(result);
 };
 
 const addReview = async (req, res) => {
-    await promisePool.execute(
-        'INSERT INTO reviews (user_id, review_content) VALUES (?, ?)', // mitä tulee review idks ?
-        [req.body.user_id, req.body.review_content]
-    );
-    res.sendStatus(201);
+	await promisePool.execute(
+		'INSERT INTO reviews (user_id, review_content) VALUES (?, ?)', // mitä tulee review idks ?
+		[req.body.user_id, req.body.review_content]
+	);
+	res.sendStatus(201);
 };
 
 const getCart = (req, res) => {
-    res.send({message: 'getting cart'});
-    // ottaa cartin jostain
+	res.send({message: 'getting cart'});
+	// ottaa cartin jostain
 };
 
 const addToCart = (req, res) => {
-    res.send({message: 'adding to cart'});
-    // laittaa jotain carttiin johonki
+	res.send({message: 'adding to cart'});
+	// laittaa jotain carttiin johonki
 };
 
 const removeFromCart = (req, res) => {
-    res.send({message: 'removing from cart'});
-    // poistetaan cartista
+	res.send({message: 'removing from cart'});
+	// poistetaan cartista
 };
 
 const order = (req, res) => {
-    res.send({message: 'ordering'});
-    // ???
+	res.send({message: 'ordering'});
+	// ???
 };
 
 export {
-    getList,
-    getReviews,
-    addReview,
-    getCart,
-    addToCart,
-    removeFromCart,
-    order,
+	getList,
+	getReviews,
+	addReview,
+	getCart,
+	addToCart,
+	removeFromCart,
+	order,
 };
