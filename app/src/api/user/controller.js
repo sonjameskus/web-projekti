893511aa5dc4a8dcf794bc20d51918bf8c61f299
utta <111429116+Uttamoosa@users.noreply.github.com>
2026-05-04 13:52:15 +0300

    signin fix

--- app/src/api/user/controller.js
+++ app/src/api/user/controller.js
@@ -5,65 +5,66 @@ import bcrypt from 'bcrypt';
 import 'dotenv/config';
 
 const login = async (req, res) => {
-    const username = req.body.username;
+	const username = req.body.username;
 
-    const user = await findUserByUsername(username);
-    if (!user) {
-        return res.status(401).send('User not found');
-    }
+	const user = await findUserByUsername(username);
+	if (!user) {
+		return res.status(401).send('User not found');
+	}
 
-    const match = await bcrypt.compare(req.body.password, user.password);
-    if (!match) {
-        res.sendStatus(401).send('Password incorrect');
-        return;
-    }
+	const match = await bcrypt.compare(req.body.password, user.password);
+	if (!match) {
+		res.sendStatus(401).send('Password incorrect');
+		return;
+	}
 
-    const webUser = {
-        user_id: user.user_id,
-        username: user.username,
-        email: user.email,
-        allergies: user.allergies,
-    };
+	const webUser = {
+		user_id: user.user_id,
+		username: user.username,
+		email: user.email,
+		allergies: user.allergies,
+	};
 
-    const key = process.env.JWT_SECRET;
-    const token = jwt.sign({user_id: user.user_id}, key, {expiresIn: '1h'});
-    res.json({user: webUser, token});
+	const key = process.env.JWT_SECRET;
+	const token = jwt.sign({user_id: user.user_id}, key, {expiresIn: '1h'});
+	res.json({user: webUser, token});
 };
 
 const logout = (req, res) => {
-    res.send({message: 'logging out'});
-    // logouttaa
+	res.send({message: 'logging out'});
+	// logouttaa
 };
 
 const signin = async (req, res) => {
-    const username = req.body.username;
-    const pass = req.body.password;
-    const email = req.body.email;
+	const username = req.body.username;
+	const pass = req.body.password;
+	const email = req.body.email;
+	console.log(123);
 
-    const user = findUserByUsername(username);
-    if (user) {
-        return res.status(401).send('User already exists');
-    }
+	const user = await findUserByUsername(username);
+	if (user) {
+		return res.status(401).send('User already exists');
+	}
 
-    bcrypt.genSalt(10, function (err, salt) {
-        bcrypt.hash(pass, salt, async function (err, hash) {
-            await promisePool.execute(
-                'INSERT INTO users (username, password, email) VALUES (?, ?, ?)',
-                [username, hash, email]
-            );
-        });
-    });
+	bcrypt.genSalt(10, function (err, salt) {
+		bcrypt.hash(pass, salt, async function (err, hash) {
+			await promisePool.execute(
+				'INSERT INTO users (username, password, email) VALUES (?, ?, ?)',
+				[username, hash, email]
+			);
+		});
+	});
 
-    res.send({message: 'Account created'});
+	res.send({message: 'Account created'});
 };
 
 const getme = async (req, res) => {
-    console.log('getMe', res.locals.user);
-    if (res.locals.user) {
-        res.json({message: 'token ok', user: res.locals.user});
-    } else {
-        res.sendStatus(401);
-    }
+	console.log('getMe', res.locals.user);
+	if (res.locals.user) {
+		res.json({message: 'token ok', user: res.locals.user});
+	} else {
+		res.sendStatus(401);
+	}
 };
 
 export {login, logout, signin, getme};
