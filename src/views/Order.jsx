d1d9c706cdc65c9e81256adcbd5a47bca4c0f3a3
sonjameskus <sonja.meskus@metropolia.fi>
2026-05-06 14:21:41 +0300

    order sivu

--- src/views/Order.jsx
+++ src/views/Order.jsx
@@ -1,14 +1,25 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import Navigation from '../components/Navigation';
+import {useMenu} from '../hooks/apiHooks';
 
 const Order = () => {
-  const [menu, setMenu] = useState([
-    {id: 1, name: 'Bibimbap', price: 12, allergens: ['gluten']},
-    {id: 2, name: 'Kimchi stew', price: 10, allergens: []},
-  ]);
-  const [openItemId, setOpenItemId] = useState(null);
-;
+  const {getMenu} = useMenu();
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchMenu = async () => {
+      try {
+        const res = await getMenu();
+        setData(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchMenu();
+  }, []);
+
+  const [openListId, setOpenListId] = useState(null);
   const [inputs, setInputs] = useState({
     description: '',
   });
@@ -19,7 +30,7 @@ const Order = () => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    setOpenItemId(false);
+    setOpenListId(false);
   };
 
   return (
@@ -35,41 +46,39 @@ const Order = () => {
       <div className="column">
         <h2>Menu</h2>
 
-        {menu.map((item) => (
-          <div key={item.id}>
-            <h3>{item.name}</h3>
-            <p>{item.price}€</p>
-
+        {data.map((list) => (
+          <div key={list.meal_id}>
+            <h2>{list.meal_name}</h2>
+            <p>{list.price} €</p>
             <button
               onClick={() =>
-                setOpenItemId(openItemId === item.id ? null : item.id)
+                setOpenListId(openListId === list.meal_id ? null : list.meal_id)
               }
             >
               Lisää koriin
             </button>
-
-            {openItemId === item.id && (
+            {openListId === list.meal_id && (
               <div className="inline-modal">
-                <h4>{item.name}</h4>
-
+                <h4>{list.meal_name} {list.price} €</h4>
+                <p>{list.meal_content}</p>
+                <p>{list.allergens}</p>
                 <form onSubmit={handleSubmit}>
                   <label>Lisätiedot (esim. allergiat yms):</label>
                   <br />
                   <textarea
-                    name="description"
+                    className="description"
                     rows={4}
                     onChange={handleInputChange}
                   />
                   <br />
 
-                  <button type="submit" onClick={() => setOpenItemId(false)}>
-                    Lisää koriin
+                  <button type="submit" onClick={() => setOpenListId(false)}>
+                    Tilaa
                   </button>
                 </form>
-
-                <button onClick={() => setOpenItemId(false)}>Sulje</button>
               </div>
             )}
+            <hr />
           </div>
         ))}
       </div>
