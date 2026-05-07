faafe7599985733b642c283aed8dadeccd006668
utta <111429116+Uttamoosa@users.noreply.github.com>
2026-05-07 15:01:24 +0300

    address

--- app/src/utils/database.js
+++ app/src/utils/database.js
@@ -2,14 +2,14 @@ import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 	port: process.env.DB_PORT || 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
 });
 
 export default pool;
